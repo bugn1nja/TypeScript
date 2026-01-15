5baa18c9e0a34f89297d649adeb8e83f13ec865a
Mujaddid Chowdhury <mujaddid.m.chowdhury@gmail.com>
2026-01-16 05:39:20 +0600

    added

---
+++ Learn_TS_from Zero/FunctionsInJS.js
@@ -0,0 +1,5 @@
+function studentInfo(name, age, grade) {
+    return `Name: ${name}, Age: ${age}, Grade: ${grade}`;
+}
+console.log(studentInfo("Alice", 20, "A")); // Output: Name: Alice, Age: 20, Grade: A
+
