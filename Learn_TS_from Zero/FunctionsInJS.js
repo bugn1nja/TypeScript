8d156359f6fe86617e23d4d2e6c5d820c47ada0d
Mujaddid Chowdhury <mujaddid.m.chowdhury@gmail.com>
2026-01-17 05:28:50 +0600

    added

--- Learn_TS_from Zero/FunctionsInJS.js
+++ Learn_TS_from Zero/FunctionsInJS.js
@@ -1,5 +1,74 @@
-function studentInfo(name, age, grade) {
-    return `Name: ${name}, Age: ${age}, Grade: ${grade}`;
-}
-console.log(studentInfo("Alice", 20, "A")); // Output: Name: Alice, Age: 20, Grade: A
+// Function is a block of code designed to perform a particular task.
 
+// Function Syntax
+function functionName(parameters) {
+  // code to be executed
+}
+// calling the function
+functionName(arguments);
+
+// Example of a function that adds two numbers
+function add(a, b) {
+  return a + b;
+}
+let sum = add(5, 10);
+console.log("Sum:", sum); // Output: Sum: 15
+
+function greet(name) {
+    return "Hello, " + name + "!"; 
+}
+let message = greet("Developer");
+console.log(message); // Output: Hello, Developer
+// 
+
+// Perameters vs Arguments
+// Parameters are the names listed in the function definition.
+// Arguments are the real values passed to the function.
+
+function greetUser(firstName, LastName) { // firstName and LastName are parameters
+    console.log("Hello , " + firstName  + LastName);
+}
+greetUser("Mujaddid ", "Chowdhury"); // "Mujaddid" and "Chowdhury" are arguments
+    
+// default parameters
+function multiply(a, b = 2) { // b has a default value of 2
+   console.log(a * b);
+   
+}   
+
+// IF THE SECOND ARGUMENT IS NOT PROVIDED, THE DEFAULT VALUE WILL BE USED
+
+multiply(5, 5); // b will be 2
+
+// Function Retunrs
+// A function can return a value using the return statement.
+//If we add return statement, additionally we have to print the function using console.log
+
+// PRACTICE EXERCISE
+
+function fn1(a) {
+    function fn2(b) {
+        return a *b;
+    }
+    return fn2;
+}
+
+let result = fn1(5); // returns fn2
+console.log(result); // Output: [Function: fn2]
+console.log(result(4)); // Output: 20 (5 * 4)
+
+// Arrow Functions
+// Arrow functions provide a more concise syntax for writing functions. 
+
+const sayHello = () => {
+    console.log("Hello!");
+}
+
+sayHello(); // Output: Hello!
+
+
+// One-liner arrow function
+
+const addNum = (x, y) => x + y;
+
+console.log(addNum(3, 7)); // Output: 10
---
+++ Learn_TS_from Zero/TemperatureConverter.js
@@ -0,0 +1,9 @@
+console.log("Welcome to Temperature Converter");
+
+let celsius = 18;
+
+function ctof(celsius) {
+    return fahrenheit = (celsius * 9/5) + 32;
+}
+
+console.log(celsius + "°C is equal to " + ctof(celsius) + "°F");
