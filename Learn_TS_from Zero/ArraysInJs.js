cb248462434b930e1b8e77667c9e41140b59ea2a
Mujaddid Chowdhury <mujaddid.m.chowdhury@gmail.com>
2026-01-27 04:14:14 +0600

    added

---
+++ Learn_TS_from Zero/ArraysInJs.js
@@ -0,0 +1,10 @@
+// Arrays in JavaScript
+// An array is a collection of items
+// It can hold multiple values in a single variable
+
+let mark_student1 = 98;
+let mark_student2 = 85;
+let mark_student3 = 76;
+let mark_student4 = 88;
+let mark_student5 = 90;
+
