f2179bce2c4d4905cc08bdfbcb71553670d2f24e
Mujaddid Chowdhury <mujaddid.m.chowdhury@gmail.com>
2026-01-27 05:52:12 +0600

    added

--- Learn_TS_from Zero/ArraysInJs.js
+++ Learn_TS_from Zero/ArraysInJs.js
@@ -8,3 +8,27 @@ let mark_student3 = 76;
 let mark_student4 = 88;
 let mark_student5 = 90;
 
+// using objects to store multiple values
+let marks = {
+    std1 : 95,
+    std2 : 85,
+    std3 : 76,
+    std4 : 88,
+    std5 : 90
+};  
+
+// using arrays to store multiple values
+
+let marks_array = [95, 85, 76, 88, 90];
+
+console.log(marks_array);
+
+console.log(marks_array.length); // length of the array
+
+// Accessing elements in the array
+
+console.log("Mark of student 1 is: " + marks_array[0]);
+console.log("Mark of student 3 is: " + marks_array[2]);
+
+marks_array[4] = 92; // updating value at index 4
+console.log("Updated mark of student 5 is: " + marks_array[4]);
