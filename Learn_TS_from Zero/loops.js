4bba298457ea10f36f9f660700209e3edf63cc41
Mujaddid Chowdhury <mujaddid.m.chowdhury@gmail.com>
2026-01-29 01:46:12 +0600

    added

---
+++ Learn_TS_from Zero/loops.js
@@ -0,0 +1,9 @@
+// Loops in JavaScript
+// Loops are used to execute a block of code repeatedly until a specified condition is met.
+
+// there are several types of loops in JavaScript:
+// 1. for loop
+// 2. while loop
+// 3. do...while loop
+// 4. for...in loop
+// 5. for...of loop
