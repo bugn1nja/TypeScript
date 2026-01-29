064e74aaed6767f305edbe15b417c06a144f0688
Mujaddid Chowdhury <mujaddid.m.chowdhury@gmail.com>
2026-01-29 18:04:04 +0600

    added

--- Learn_TS_from Zero/Scopes_&Hosting.js
+++ Learn_TS_from Zero/Scopes_&Hosting.js
@@ -4,11 +4,11 @@
 // Variables declared outside of any function or block have global scope
 let globalVar = "I am a global variable";
 function showGlobalVar() {
-    console.log(globalVar); // Accessible here
+    // console.log(globalVar); // Accessible here
 }
 
 showGlobalVar();
-console.log(globalVar); // Accessible here too
+// console.log(globalVar); // Accessible here too
 
 // 2. Function Scope
 // Variables declared within a function are only accessible within that function
@@ -16,14 +16,14 @@ function myFunction() {
     let functionVar = "I am a function variable";
     console.log(functionVar); // Accessible here
 }
-myFunction();
+// myFunction();
 // console.log(functionVar); // Unaccessible here, will throw an error
 
 // 3. Block Scope
 // Variables declared with let or const within a block (e.g., inside { }) are only accessible within that block
 if (true) {
     let blockVar = "I am a block variable";
-    console.log(blockVar); // Accessible here
+    // console.log(blockVar); // Accessible here
 }
 // console.log(blockVar); // Unaccessible here, will throw an error
 
@@ -33,5 +33,35 @@ function outerFunction() {
     function innerFunction() {
         console.log(outerVar); // Accessible here due to lexical scope
     }
-    innerFunction();
+    // innerFunction();
 }
+
+// outerFunction();
+
+if (true) {
+    const username = "Mujaddid";
+    if (username === "Mujaddid") {
+        const welcomeMessage = "Welcome back, " + username + "!";
+        // console.log(welcomeMessage); // Accessible here
+    }
+    // console.log(welcomeMessage);
+}
+// console.log(username);
+
+// 5. Hoisting
+// In JavaScript, variable and function declarations are "hoisted" to the top of their containing scope
+
+console.log(addOne(5)); // Outputs: 6
+function addOne(number) {
+    return number + 1;
+}
+
+const addTwo = function(number)
+{
+    return number + 2;
+}
+console.log(addTwo(5)); // Outputs: 7
+
+
+
+
