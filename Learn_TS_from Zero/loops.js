d64b0e5ea1f717595798524ea0acc5622e877374
Mujaddid Chowdhury <mujaddid.m.chowdhury@gmail.com>
2026-01-29 06:36:11 +0600

    added

--- Learn_TS_from Zero/loops.js
+++ Learn_TS_from Zero/loops.js
@@ -7,3 +7,31 @@
 // 3. do...while loop
 // 4. for...in loop
 // 5. for...of loop
+
+
+// 1. for loop
+for (let i = 0; i < 5; i++) {
+    console.log("For Loop iteration:", i);
+}
+
+// in this example, the loop will run 5 times, printing the value of i from 0 to 4.
+// first we initialize i to 0, then we check if i is less than 5, and after each iteration, we increment i by 1.
+
+// 2. while loop
+let j = 0;
+while (j < 5) {
+    console.log("While Loop iteration:", j);
+    j++;
+}
+// in this example, the loop will run as long as j is less than 5.
+// we initialize j to 0, and in each iteration, we print the value of j and increment it by 1.
+
+// 3. do...while loop
+let k = 0;
+do {
+    console.log("Do...While Loop iteration:", k);
+    k++;
+} while (k < 5);
+// in this example, the loop will run at least once, and then continue to run as long as k is less than 5.
+// we initialize k to 0, and in each iteration, we print the value of k and increment it by 1.
+
