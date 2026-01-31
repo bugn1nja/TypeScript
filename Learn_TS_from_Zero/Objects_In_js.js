5cc8a9faf526a06f8ab48221e27fdf9cbfdfff7a
Mujaddid Chowdhury <mujaddid.m.chowdhury@gmail.com>
2026-01-31 10:52:12 +0600

    added

---
+++ Learn_TS_from_Zero/Objects_In_js.js
@@ -0,0 +1,23 @@
+// Objects in JavaScript
+
+// An object is a collection of key-value pairs
+// It is used to store related data and functionality together
+
+// Creating an Object
+let person = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+
+// accessing Object Properties
+ 
+console.log("The name of stored person is : " + person.name)
+console.log("The age of stored person is : " + person.age)
+console.log("The city of stored person is : " + person.city)
+
+console.log("Persons All data is : ", person);
+
+// Modifying Object Properties
+person.age = 31;
+console.log("After modifying, the age of stored person is : " + person.age)
