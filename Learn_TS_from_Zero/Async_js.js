ec7ef8f024765567484cd228823ca09aa4684559
Mujaddid Chowdhury <mujaddid.m.chowdhury@gmail.com>
2026-02-09 15:41:26 +0600

    added

--- Learn_TS_from_Zero/Async_js.js
+++ Learn_TS_from_Zero/Async_js.js
@@ -1,14 +1,30 @@
 function hello() {
-    console.log("Hello World");
+    console.log("Hi");
 }
 
-setTimeout(hello, 2000); // 2 seconds later, it will print "Hello World"
+setTimeout(hello, 2000); // 1 second later, it will print "Hello World"
 
 console.log("This will print first"); // This will print before "Hello World" because setTimeout is asynchronous
 
 setTimeout(() => {
-    console.log("This will print 5 seconds after the first message");
-}, 5000); // This will print after 5 seconds
+    console.log("This will print 3 seconds after the first message");
+}, 3000); // This will print after 1 seconds
 
 console.log("Mujaddid")
 
+let Counter = 1;
+function getdata(dataID, nextdata) {
+    setTimeout(() => {
+        console.log(" Data ID " + Counter + " : " + dataID);
+        Counter++;
+        if (nextdata) {
+            nextdata();
+        }
+    }, 4000);
+    
+}
+getdata(243 , () => {
+    getdata(489, () => {
+        getdata(554);
+    });
+});
