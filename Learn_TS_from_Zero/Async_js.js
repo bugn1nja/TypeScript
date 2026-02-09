01234badf83d56f53ed8b9972b2e767d3b5bb325
Mujaddid Chowdhury <mujaddid.m.chowdhury@gmail.com>
2026-02-09 20:15:58 +0600

    added

--- Learn_TS_from_Zero/Async_js.js
+++ Learn_TS_from_Zero/Async_js.js
@@ -4,7 +4,7 @@
 
 const getPromise = () => {
     return new Promise((resolve, reject) => {
-    console.log("Promise is pending...");
+    // console.log("Promise is pending...");
     // resolve("Promise is resolved successfully!");
     reject("Promise is rejected!");
 });
@@ -12,9 +12,42 @@ const getPromise = () => {
 
 let promise = getPromise();
 
-promise.then((data) => {
-    console.log("Order is placed successfully!");
-});
-promise.catch((error) => {
-    console.log("Your order is cancelled! ");
-});
+// promise.then((data) => {
+//     console.log("Order is placed successfully!");
+// });
+// promise.catch((error) => {
+//     // console.log("Your order is cancelled! ");
+// });
+
+
+// Promise Chianing
+
+let Data_1 = "Username is correct!";
+let Data_2 = "Password is correct!";
+
+function asyncFunctoin() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Data_1 : " + Data_1);
+            resolve("Data_1 is resolved successfully!");
+        }, 3000);
+    });
+}
+
+function asyncFunctoin2() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Data_2 : " + Data_2);
+            resolve("Data_2 is resolved successfully!");
+        }, 3000);
+    });
+}
+console.log("Featching username from server...");
+asyncFunctoin().then((res) => {
+    // console.log(res);
+    console.log("Featching password from server...");
+    asyncFunctoin2().then((res) => {
+        // console.log(res);
+    });
+}
+);
