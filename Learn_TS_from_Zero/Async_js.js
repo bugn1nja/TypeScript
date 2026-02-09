ffea0a26b0bd3600ca16bbc963efb766432f581c
Mujaddid Chowdhury <mujaddid.m.chowdhury@gmail.com>
2026-02-09 18:35:09 +0600

    added

--- Learn_TS_from_Zero/Async_js.js
+++ Learn_TS_from_Zero/Async_js.js
@@ -2,8 +2,19 @@
 
 // Promise Example......................................
 
-let promise = new Promise((resolve, reject) => {
+const getPromise = () => {
+    return new Promise((resolve, reject) => {
     console.log("Promise is pending...");
     // resolve("Promise is resolved successfully!");
     reject("Promise is rejected!");
+});
+};
+
+let promise = getPromise();
+
+promise.then((data) => {
+    console.log("Order is placed successfully!");
+});
+promise.catch((error) => {
+    console.log("Your order is cancelled! ");
 });
