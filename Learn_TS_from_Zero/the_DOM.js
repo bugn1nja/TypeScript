68d70d68841028a386ce9758038e8cdad054ab86
Mujaddid Chowdhury <mujaddid.m.chowdhury@gmail.com>
2026-03-31 06:32:16 +0600

    added

--- Learn_TS_from_Zero/the_DOM.js
+++
@@ -1,9 +0,0 @@
-// DOM
-// Document Object Model
-
-// DOM is a programming interface for HTML and XML documents. It represents the page so that programs can change the document structure, style, and content. The DOM represents the document as a tree of nodes and objects; that way, programming languages can interact with the page.
-
-// The DOM is not a programming language, but without it, the JavaScript language wouldn't have any model or notion of web pages, HTML documents, and their components. The DOM is an API (Application Programming Interface) for valid HTML and well-formed XML documents.
-
-// DOM is a W3C (World Wide Web Consortium) standard. It defines a standard for accessing documents. It defines the properties of all document elements and the methods to access them. The DOM is designed to be used with any programming language, but it is most commonly used with JavaScript.
-
