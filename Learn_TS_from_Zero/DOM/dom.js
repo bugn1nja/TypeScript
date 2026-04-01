861fd85888ef38641d2ea898fbf5476aac8e3fb2
Mujaddid Chowdhury <mujaddid.m.chowdhury@gmail.com>
2026-04-01 09:31:45 +0600

    added

--- Learn_TS_from_Zero/DOM/dom.js
+++ Learn_TS_from_Zero/DOM/dom.js
@@ -1,1 +1,11 @@
-console.log("Hello, DOM!");
+// DOM 
+
+// DOM stands for Document Object Model. It is a programming interface for HTML and XML documents. It represents the page so that programs can change the document structure, style, and content. The DOM represents the document as a tree of nodes, where each node represents an element, attribute, or piece of text in the document.
+
+// In TypeScript, we can use the DOM API to manipulate the document. We can access elements, change their properties, and add event listeners to them. The DOM API is available in the global scope, so we can use it directly in our TypeScript code.
+
+
+document.getElementById("bt");
+console.log(bt);
+
+
