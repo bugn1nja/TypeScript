ca957ad50710be486e4a7ce8315844facd80919f
Mujaddid Chowdhury <mujaddid.m.chowdhury@gmail.com>
2026-04-14 20:51:55 +0600

    added

--- Learn_TS_from_Zero/DOM/dom.js
+++ Learn_TS_from_Zero/DOM/dom.js
@@ -4,8 +4,12 @@
 
 // In TypeScript, we can use the DOM API to manipulate the document. We can access elements, change their properties, and add event listeners to them. The DOM API is available in the global scope, so we can use it directly in our TypeScript code.
 
+console.log("Hello, DOM!");
+console.log(window); // The global window object
+window.console.log("hello2")
 
-document.getElementById("bt");
-console.log(bt);
+// console.log() is window onject's method, so we can also use it as window.console.log().
 
+console.dir(document.body); // The body element of the document
+console.dir(document.body.childNodes[1]); // The child nodes of the body element
 
